b9df0a7206c13779d6d60ead8f884e771ba8f7ea
dhananjay.patil <dhananjay.patil@alixpartners.com>
2023-12-14 18:29:14 +0530

    UPDATE: Added ApplicationDBContext

---
+++ Data1/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Blazor_Practice_1.Model1;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blazor_Practice_1.Data1
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Developer> Developers { get; set; }
+    }
+}
